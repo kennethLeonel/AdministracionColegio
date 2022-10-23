ca0137490e397515bdfe3ce87514ec6ec8a1bfbd
Kenneth David Leonel Triana <60140555+kennethLeonel@users.noreply.github.com>
2022-10-22 22:56:34 -0500

    Se realizó Menú, rectificar en Cel

--- controllers/controladorIndex.js
+++ controllers/controladorIndex.js
@@ -1,24 +1,34 @@
 const empleados = require('../db/archivoEmpleador.json');
-const Swal = require('sweetalert2')
+
+const store = require('store')
 const controller = {
 
     vista: (req, res) => {
         res.render('../views/index');
     },
     realizarLogin: (req, res) => {
-        console.log("hola",req.body);
+        
         let usuario = req.body.usuario;
         let contra = req.body.contra;
-        console.log(usuario);
-        console.log(contra);
+  
         let empleado = empleados.find(empleado => empleado.usuario == usuario && empleado.contra == contra);
-        console.log(empleado);
-        if(empleado != undefined){   
-        res.render('./formularioEmpleado', {empleadoLogin: empleado});
+      
+        if(empleado != undefined){      
+         store.set('usuario', {usuario: empleado.usuario});
+         store.set('sede',{sede: empleado.sede});
+      
+        res.render('./formularioEmpleado', {empleadoLogin: empleado , sede: store.get('sede').sede} );
         }else{
             res.render('./index', {error: "Usuario o contraseña incorrectos"});
         }
-        }
+        },
+    cerrar :(req, res) => {
+        store.clearAll();
+        res.render('./index');
+    },
+    vistaEmpleado : (req, res) => {
+        res.render('./formularioEmpleado');
+    }
     }
 
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,5 +5,7 @@ const controladorAdmin = require ('../controllers/controladorIndex')
 
 router.get('/', controladorAdmin.vista);
 router.post('/', controladorAdmin.realizarLogin);
+router.get('/formularioEmpleado', controladorAdmin.vistaEmpleado);
+router.get('/logout', controladorAdmin.cerrar);
 
 module.exports = router
